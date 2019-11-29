eb1e1be03a0383fa3bc9990886ab8c0380a64334
rigox <rigoc1994@gmail.com>
2019-11-28 22:56:52 -0500

    a

--- models/Profile.js
+++ models/Profile.js
@@ -3,6 +3,31 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
 
+user:{
+     type:Schema.Types.ObjectId,
+     ref:'users',
+     books:[    
+     ],
+
+ books:[
+     {
+          type:Schema.Types.ObjectId,
+          ref:'books'
+     }
+ ],
+
+ amountDue:{
+      type:Number,
+      default:0
+ },
+
+createdAt:{
+      type:Date,
+      default:  Date.now()
+}
+}
+
+
 });
 
 
